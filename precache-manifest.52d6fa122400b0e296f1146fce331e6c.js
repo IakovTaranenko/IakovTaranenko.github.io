57235d75d53f083890a1903034bca2d95b7d55d4
IakovTaranenko <IakovTaranenko@users.noreply.github.com>
2021-02-15 21:40:28 -0800

    Updates

---
+++ precache-manifest.52d6fa122400b0e296f1146fce331e6c.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa527688f6db333ee9d142660f39d7a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82152cd8d92a6b6f5e5d",
+    "url": "/static/js/main.61159076.chunk.js"
+  },
+  {
+    "revision": "7fddde83a9c5bd79c566",
+    "url": "/static/js/runtime-main.6d6a991c.js"
+  }
+]);
---
+++ static/js/main.61159076.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpmain=this.webpackJsonpmain||[]).push([[0],[function(n,i,p){n.exports=p(1)},function(n,i){}],[[0,1]]]);
+//# sourceMappingURL=main.61159076.chunk.js.map
